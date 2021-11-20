6ebd762cb395232360c65a80d70d1e3f52a4746b
Ezhilarasi <tezzhilarasi@gmail.com>
2021-11-20 20:24:26 +0530

    back to initial

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const bodyParser = require("body-parser")
 const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require("spotify-web-api-node")
 
